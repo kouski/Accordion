ea18069e09084f66211dc290b7a6052b197d891f
kouski <72550619+kouski@users.noreply.github.com>
2025-02-04 21:14:59 +0100

    crear logica para acordeon,usando el id

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import questions from "./data";
+
 const App = () => {
-  return <h2>Accordion Starter</h2>;
+  const [activeId, setActiveId] = useState(null);
+
+  function toggle(id) {
+    // Si la pregunta ya está abierta, la cierra; si no, la abre
+    setActiveId(activeId === id ? null : id);
+  }
+
+  return (
+    <main>
+      <div className="container">
+        <h1>Questions</h1>
+
+        {questions.map((question) => (
+          <div className="question" key={question.id}>
+            <header className="question">
+              <h5>{question.title}</h5>
+              <button
+                onClick={() => toggle(question.id)}
+                type="button"
+                className="question-btn"
+              >
+                {activeId === question.id ? "−" : "+"}
+              </button>
+            </header>
+
+            {/* Solo muestra el texto si el ID activo coincide con el de la pregunta */}
+            {activeId === question.id && <p>{question.info}</p>}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 };
+
 export default App;
